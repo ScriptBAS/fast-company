e62076a22984b52958f7a88f3a837715fc271efe
Rustam Akhmetov <scriptbas@yandex.ru>
2023-01-29 21:58:19 +0500

    task 20-2 completed

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Redirect, useParams } from "react-router-dom";
 import EditUserPage from "../components/page/editUserPage";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
-import UserProvider from "../hooks/useUsers";
+import UsersLoader from "../components/ui/hoc/usersLoader";
+import { getCurrentUserId } from "../store/users";
 export const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
+    const currentUserid = useSelector(getCurrentUserId());
+
     return (
-        <UserProvider>
-            {userId ? (
-                edit ? (
-                    <EditUserPage />
+        <UsersLoader>
+                {userId ? (
+                    edit ? (
+                        userId === currentUserid ? (
+                            <EditUserPage />
+                        ) : (
+                            <Redirect to={`/users/${currentUserid}/edit`} />
+                        )
+                    ) : (
+                        <UserPage userId={userId} />
+                    )
                 ) : (
-                    <UserPage userId={userId} />
-                )
-            ) : (
-                <UsersListPage />
-            )}
-        </UserProvider>
+                    <UsersListPage />
+                )}
+        </UsersLoader>
     );
 };
 
